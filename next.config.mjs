040c5b9b64c43c9b6ba87082a416e223faa54544
Juanma <camachosanchezjm@gmail.com>
2026-02-26 13:15:39 +0100

    Fix build for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/coreLiveDemo',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
